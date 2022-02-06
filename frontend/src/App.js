da40e8697cef3a6c95aaddb4a9f973cd98bf5088
Casey Cochran <Casey3362@yahoo.com>
2022-02-05 19:50:28 -0800

    userspots component added and state modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, useParams } from "react-router-dom";
 import { restoreUser } from "./store/session";
 import SignupFormPage from "./components/SignUpFormModal";
 import Navigation from "./components/Navigation";
 import CreateUserSpots from "./components/Spots/CreateUserSpots";
+import UserSpots from "./components/UserSpots/UserSpots";
 
 function App() {
   const dispatch = useDispatch();
+  const {userId} = useParams()
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(restoreUser()).then(() => setIsLoaded(true));
@@ -23,6 +25,9 @@ function App() {
           </Route>
           <Route path='/api/users/spots/new'>
             <CreateUserSpots />
+          </Route>
+          <Route path={`/api/users/userId/spots`}>
+            <UserSpots />
           </Route>
         </Switch>
       )}
---
+++ frontend/src/components/UserSpots/UserSpots.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+
+
+
+const UserSpots = () => {
+    const userId = useParams();
+
+    return (
+        <div>
+            <h1>hellop from specific users spot listings</h1>
+        </div>
+    )
+}
+
+
+
+
+
+
+export default UserSpots;

b142328998c2adbb11ecace869dc867c3978f1ef
Casey Cochran <Casey3362@yahoo.com>
2022-02-09 18:49:34 -0800

    home page styled, create spot page styled

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -15,20 +15,20 @@ const HomePage = () => {
         <h2 className="insp-header">Inspiration for your next trip</h2>
         <div className="inspiration">
           <div className="insp-box-children one">
-            <img className="testingthis" src='https://m.media-amazon.com/images/I/71h-waAyTVL._AC_SY606_.jpg' />
-            <p className="testingpls">box bottom</p>
+            <img src='https://m.media-amazon.com/images/I/71h-waAyTVL._AC_SY606_.jpg' />
+            <p className="insp-box-text">Seattle</p>
           </div>
           <div className="insp-box-children two">
             <img src='https://m.media-amazon.com/images/I/81-NfKLyOYL._AC_SL1500_.jpg' />
-            <p>boxes bottom</p>
+            <p className="insp-box-text">Moscow</p>
           </div>
           <div className="insp-box-children three">
-            <img src='https://m.media-amazon.com/images/I/81fq1Y6W+9L._AC_SL1500_.jpg' />
-            <p>boxes bottom</p>
+            <img src='https://m.media-amazon.com/images/I/71+ViZbDVCL._AC_SY450_.jpg' />
+            <p className="insp-box-text">New York City</p>
           </div>
           <div className="insp-box-children four">
             <img src='https://m.media-amazon.com/images/I/712Vyiz8ToL._AC_SY679_.jpg' />
-            <p>boxes bottom</p>
+            <p className="insp-box-text">San Francisco</p>
           </div>
         </div>
       </div>
@@ -37,13 +37,10 @@ const HomePage = () => {
         <img src='https://visitutahkenticoprod.blob.core.windows.net/cmsroot/visitutah/media/site-assets/three-season-photography/mighty-5/zion/zion-national-park_the-narrows_istock.jpg' alt='narrows picture' />
         <img src='https://i.pinimg.com/originals/09/2b/c4/092bc4a3192ecd5ae9589be9a0d8ab90.jpg' alt='rustic cabin' />
       </div>
+      <h2 className="insp-header"> Learn More About Me</h2>
       <div className="container-children four">
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1>test</h1>
-        <h1></h1>
-      </div>
-      <div className="container-children">
+        <h3>github link</h3>
+        <h1>linked in</h1>
         <h1>test</h1>
       </div>
     </div>

97ed0814f9dbf78298d28da9b5b4926041d9f381
Casey Cochran <Casey3362@yahoo.com>
2022-02-11 10:53:58 -0800

    fixed bookings to only show user specific bookings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
 
 
 const sessionUser = useSelector((state) => state.session.user);
-console.log(sessionUser, '-------session user routes')
+
 
 
   return (
--- frontend/src/components/UserBookings/UserBookings.js
+++ frontend/src/components/UserBookings/UserBookings.js
@@ -6,25 +6,32 @@ import { fetchUserSpots } from "../../store/spotReducer";
 
 
 const UserBookings = () => {
-    //const {userId} = useParams();
+    const {userId} = useParams();
     const dispatch = useDispatch();
-    const user = useSelector((state) => state.session.user)
-    const userId = user?.id
+    const sessionUser = useSelector((state) => state.session.user);
     const bookings = useSelector((state) => Object.values(state.bookingsReducer.booking))
-
+    const userBookings = bookings?.filter(booking =>  +userId === booking.userId)
 
     useEffect(() => {
         dispatch(loadBooking(userId));
         dispatch(fetchUserSpots(userId))
+    },[dispatch])
 
-    },[])
+
+  if (sessionUser && sessionUser?.id !== +userId) {
+    return <Redirect to="/" />;
+  }
+
+  if (!sessionUser) {
+    return <Redirect to="/" />;
+  }
 
 
     return (
         <div>
             <h1>hello from here </h1>
             <ul>
-                {bookings?.map((booking,i) => <li key={i}>{booking.startDate}{booking?.Spot?.name}
+                {userBookings?.map((booking,i) => <li key={i}>{booking.startDate}{booking?.Spot?.name}
                 <button onClick={(() => dispatch(cancelBooking(booking.id, userId)))}>Cancel Booking</button></li>)}
 
             </ul>
